da9336f7587eb9bd58e4802d89fa899f6cab28a0
Deepthi Rao <dgrao2@ncsu.edum>
2018-04-25 00:02:13 -0400

    adding todos to complete in state and statemachine

---
+++ Project2/machine_abstractions/statemachine.py
@@ -0,0 +1,28 @@
+from rules_abstractions.threeCardRule import ThreeCardRule
+from rules_abstractions.fourCardRule import FourCardRule
+from rules_abstractions.twoCardRule import TwoCardRule
+from machine_abstractions.state import State
+
+# This is the abstraction for the state machine that will be in each game
+
+class StateMachine:
+
+    #initializes the state machine with a name, the game being passed in and the deck
+    def __init__(self, name, game, players, rules, deck):
+        self.name = name
+        self.game = game
+        self.players = players
+        self.rules = rules
+        self.deck = deck #with stack abstraction properties
+        self.pile = None
+
+        #current player default always set to first player in array
+        self.currentPlayer = players[0]
+
+        #
+        self.currentState # = Start state
+
+
+    # TODO: make a start state
+    # make an end state and make getters and setters. 
+
