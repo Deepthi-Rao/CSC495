6e29831c12373034f70dff58f833624661cf6dbd
hfernan <hfernan@ncsu.edu>
2018-04-25 19:59:41 -0400

    Added some comments

--- Project2/persistent/deck.py
+++ Project2/persistent/deck.py
@@ -11,18 +11,23 @@ class Deck(Stack):
             self.stack.append(Card("Joker", None))
             self.stack.append(Card("Joker", None))
         
+    # deals a specified number of cards from the top of the deck
     def deal(self, numCards):
         return [self.pop() for _ in range(numCards)]
 
+    # takes a list of cards to shuffle back into the deck
     def shuffleInCards(self, cards):
         self.stack.extend(cards)
         self.shuffle()
 
+    # returns false if the deck is empty
     def hasCards(self):
         return self.size() > 0
 
+    # returns the top card of the deck
     def draw(self):
         return self.pop()
 
+    # returns the number of cards remaining in the deck
     def getNumCards(self):
         return self.size()
