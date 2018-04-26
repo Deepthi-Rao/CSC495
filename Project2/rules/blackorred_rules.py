33d63ab0f276d5db56682c48e93797b4e7cfdaec
hfernan <hfernan@ncsu.edu>
2018-04-25 21:50:13 -0400

    putting BoR rules in the right module.... whoops

--- Project2/rules/blackorred_rules.py
+++ Project2/rules/blackorred_rules.py
@@ -1,3 +1,17 @@
-'''
+from rules_abstractions.twoCardRule import TwoCardRule
 
-'''
+def getColor(card):
+    if card.getSuit() == "Hearts" or card.getSuit() == "Diamonds":
+        return "RED"
+    else:
+        return "BLACK"
+
+# Guessing is the only play in Black Or Red
+class GuessRule(TwoCardRule):
+    
+    # compCard is the drawn card
+    # guess is the player's guess of its color
+    def canPlay(self, compCard, guess):
+        if getColor(compCard) == guess.upper():
+            return True
+        return False
